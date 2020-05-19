2ac713b7ff7fa7efffe78e7df45bc5dc4e99fdaa
akinayturan <akinayturan@gmail.com>
2020-05-19 21:02:41 +0300

    init 3 @serbayacar change to loking lines in text

--- program.py
+++ program.py
@@ -5,8 +5,7 @@ import xlsxwriter
 globalExcelFileName = 'test.xlsx';
 globalFilePath = "datas/*.txt";
 
-
-def createExcelFile( bookName , data ):   
+def createExcelFile(bookName, data):
     # Create a workbook and add a worksheet.
     workbook = xlsxwriter.Workbook(bookName)
     worksheet = workbook.add_worksheet()
@@ -19,50 +18,40 @@ def createExcelFile( bookName , data ):
     # for col, data in enumerate(data):
     #     worksheet.write_column(row, col, data)
 
-    for date, time, con, zfive, one, twoFive, five, ten, atrh, dpwh  in data:
-            worksheet.write(row, col,  date)
-            worksheet.write(row, col+1,  time)
-            worksheet.write(row, col+2,  con)
-            worksheet.write(row, col+3,  zfive)
-            worksheet.write(row, col+4,  one)
-            worksheet.write(row, col+5,  twoFive)
-            worksheet.write(row, col+6,  five)
-            worksheet.write(row, col+7,  ten)
-            worksheet.write(row, col+8,  atrh)
-            worksheet.write(row, col+9,  dpwh)
-            row += 1
-
+    for row_num, item in enumerate(data):
+        for row_num2, item2 in enumerate(item):
+            worksheet.write(col, row_num2, item2)
+        col = col +1
     workbook.close()
 
 
-
-def getRowData( filePath ):
+def getRowData(filePath):
     # parsing date and time values
     baseName = os.path.basename(file)
     dateValue, hourValue = baseName.split(' ');
     hourValue = hourValue.replace(".txt", "");
 
+    lines=[dateValue, hourValue]
     # parsing file contents
     with open(filePath, 'r') as f:
-        lines = [line.rstrip() for line in f]
+        for line in f:
+            # in python 2
+            # print line
+            # in python 3
+            lines.append(line);
 
-    #return all rowData
-    return [dateValue, hourValue, lines[0],lines[1],lines[2],lines[3],lines[4],lines[5],lines[6],lines[7]];
+        f.close()
+
+    # return all rowData
+    return lines;
 
 
+allRows = [];
 
-allRows=[];
-
-txt_files = glob.glob( globalFilePath );
+txt_files = glob.glob(globalFilePath);
 
 for file in txt_files:
-    rowData= getRowData(file)
+    rowData = getRowData(file)
     allRows.append(rowData);
 
-createExcelFile( globalExcelFileName , allRows);
-
-
-
-
-
-
+createExcelFile(globalExcelFileName, allRows);
